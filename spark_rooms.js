e9978a1ed1c6a695d2fbb381f15c798d28e861ec
Hantzley Tauckoor <hantzley@gmail.com>
2017-07-02 01:28:57 +0000

    Changed Max value

--- spark_rooms.js
+++ spark_rooms.js
@@ -14,7 +14,7 @@ const options = { method: 'GET',
    {
      'cache-control': 'no-cache',
      'content-type': 'application/json; charset=utf-8',
-     max: '700',
+     max: '50',
      authorization: auth_token
    }
 };
